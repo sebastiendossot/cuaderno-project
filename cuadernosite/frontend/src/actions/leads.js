f0d77f166901931450df419f3760a0840cd5e9e2
Sebastien Dossot <sebastien.dossot@gmail.com>
2020-01-30 15:50:13 -0700

    Initial commit, still need to replace leads and change migrations

---
+++ cuadernosite/frontend/src/actions/leads.js
@@ -0,0 +1,43 @@
+import axios from 'axios';
+import {createMessage, returnErrors} from './messages';
+import { tokenConfig } from './auth';
+
+import { GET_LEADS, DELETE_LEAD, ADD_LEAD, GET_ERRORS } from './types';
+
+//GET LEADS
+ 
+export const getLeads = () => (dispatch, getState) => {
+  axios.get('/api/leads/', tokenConfig(getState))
+    .then(res => {
+      dispatch({
+        type: GET_LEADS,
+        payload: res.data
+      });
+    }).catch(err => dispatch(returnErrors(err.response.data,err.response.status)))
+}
+
+//DELETE LEAD
+
+export const deleteLead = id => (dispatch, getState) => {
+  axios.delete(`/api/leads/${id}/`, tokenConfig(getState))
+    .then(res => {
+      dispatch(createMessage({deleteLead: 'Lead deleted'}));
+      dispatch({
+        type: DELETE_LEAD,
+        payload: id
+      });
+    }).catch(err => console.log(err))
+} 
+
+//ADD LEAD
+
+export const addLead = lead => (dispatch, getState) => {
+  axios.post('/api/leads/', lead, tokenConfig(getState))
+    .then(res => {
+      dispatch(createMessage({addLead: 'Lead added'}));
+      dispatch({
+        type: ADD_LEAD,
+        payload: res.data
+      });
+    }).catch(err => dispatch(returnErrors(err.response.data,err.response.status)))
+}
